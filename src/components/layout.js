9173d136635500cef0c407c538a090bfb290b487
illulli <illu.development@gmail.com>
2018-10-12 19:17:06 +1100

    trying to add redirect script via didMount

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,45 +5,114 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
-
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+// const html = () => {
+//   return {
+//     __html: `if (window.netlifyIdentity) {
+//       window.netlifyIdentity.on("init", user => {
+//         if (!user) {
+//           window.netlifyIdentity.on("login", () => {
+//             document.location.href = "/admin/";
+//           });
+//         }
+//       });
+//     }`
+//   }
+// }
+class Layout extends React.Component{
+  componentDidMount(){
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
-)
+      });
+    }
+  }
+  render(){
+    return (
+    <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
+          }
+        `}
+        render={data => (
+          <>
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { name: 'description', content: 'Sample' },
+                { name: 'keywords', content: 'sample, something' },
+              ]}
+            >
+              <html lang="en" />
+              <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+              {/* <script dangerouslySetInnerHTML={html()} /> */}
+            </Helmet>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div
+              style={{
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '0px 1.0875rem 1.45rem',
+                paddingTop: 0,
+              }}
+            >
+              {this.props.children}
+              
+            </div>
+          </>
+        )}
+      />
+    )
+  }
+}
+// const Layout = ({ children }) => (
+//   <StaticQuery
+//     query={graphql`
+//       query SiteTitleQuery {
+//         site {
+//           siteMetadata {
+//             title
+//           }
+//         }
+//       }
+//     `}
+//     render={data => (
+//       <>
+//         <Helmet
+//           title={data.site.siteMetadata.title}
+//           meta={[
+//             { name: 'description', content: 'Sample' },
+//             { name: 'keywords', content: 'sample, something' },
+//           ]}
+//         >
+//           <html lang="en" />
+//           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+//           <script dangerouslySetInnerHTML={html()} />
+//         </Helmet>
+//         <Header siteTitle={data.site.siteMetadata.title} />
+//         <div
+//           style={{
+//             margin: '0 auto',
+//             maxWidth: 960,
+//             padding: '0px 1.0875rem 1.45rem',
+//             paddingTop: 0,
+//           }}
+//         >
+//           {children}
+           
+//         </div>
+//       </>
+//     )}
+//   />
+// )
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,

5f5359a9feac8d5fedd69064cb49d86ab1d27f63
illulli <illu.development@gmail.com>
2018-10-15 16:18:58 +1100

    social media meta added 12

--- src/components/social/GoogleData.js
+++ src/components/social/GoogleData.js
@@ -32,246 +32,148 @@ export const getGoogleArticle = (title, img, alt) => {
     }
   }
 
-  export const getGoogleRestaurant = () => {
-      return {
-        "@context":"http://schema.org",
-        "@type":"Menu",
-        "name": "Our Menu",
-        "url": "https://goofy-archimedes-763914.netlify.com/",
-        "mainEntityOfPage": "https://goofy-archimedes-763914.netlify.com/",
-        "inLanguage":"English",
-        "offers": {
+
+export const getGoogleRestaurant = () => {
+    return {
+        "@context": "http://schema.org/",
+        "@type": "Restaurant",
+        "acceptsReservations": "true",
+        "servesCuisine": "Indian",
+        "name": "Gekko",
+        "address": "1 infy loop, Mercedes Park, Vic",
+        "priceRange": "$$",
+        "telephone": "454 5454 454",
+        "image": "https://goofy-archimedes-763914.netlify.com/static/scandic-c90942cfae912bb38f91d18b04b9ba6d-566f0.jpg",
+        "hasMenu": {
+            "@type":"Menu",
+            "name": "Our Menu",
+            "url": "https://goofy-archimedes-763914.netlify.com/",
+            "mainEntityOfPage": "https://goofy-archimedes-763914.netlify.com/",
+            "inLanguage":"English",
+            "offers": {
+             "@type": "Offer",
+             "availabilityStarts": "T17:00",
+             "availabilityEnds": "T23:00"
+            },
+           "hasMenuSection": [
+            {
+             "@type": "MenuSection",
+             "name": "Appetizers",
+             "hasMenuItem": [
+             {
+              "@type": "MenuItem",
+              "name": "Fried Eggplant",
+              "description": "Served with Italian red gravy.",
+              "offers": {
          "@type": "Offer",
-         "availabilityStarts": "T17:00",
-         "availabilityEnds": "T23:00"
-        },
-       "hasMenuSection": [
-        {
-         "@type": "MenuSection",
-         "name": "Appetizers",
-         "hasMenuItem": [
-         {
-          "@type": "MenuItem",
-          "name": "Fried Eggplant",
-          "description": "Served with Italian red gravy.",
-          "offers": {
-     "@type": "Offer",
-                      "price": "7.95",
-                      "priceCurrency": "USD"
-          }
-         },
-         {
-          "@type": "MenuItem",
-          "name": "Fried Calamari",
-          "description": "Served with Italian red gravy or honey mustard.",
-             "image": "https://goofy-archimedes-763914.netlify.com/static/scandic-c90942cfae912bb38f91d18b04b9ba6d-566f0.jpg",
-          "suitableForDiet": "http://schema.org/GlutenFreeDiet",
-          "nutrition": {
-        "@type": "NutritionInformation",
-                     "calories": "573 calories",
-                      "fatContent": "25 grams",
-                      "carbohydrateContent": "26 grams",
-                      "proteinContent": "61 grams"
-                    },
-          "offers": {
-        "@type": "Offer",
-                      "price": "7.95",
-                      "priceCurrency": "USD"
-        }
-         }
-         ]
-        },
-        {
-         "@type": "MenuSection",
-         "name": "Soups",
-         "hasMenuItem": [
-         {
-          "@type": "MenuItem",
-          "name": "Lobster Bisque",
-          "offers": [
-          {
-           "@type": "Offer",
-           "price": "6.75",
-           "priceCurrency": "USD",
-           "eligibleQuantity": {
-            "@type": "QuantitativeValue",
-            "name": "Cup"
+                          "price": "7.95",
+                          "priceCurrency": "USD"
+              }
+             },
+             {
+              "@type": "MenuItem",
+              "name": "Fried Calamari",
+              "description": "Served with Italian red gravy or honey mustard.",
+                 "image": "https://goofy-archimedes-763914.netlify.com/static/scandic-c90942cfae912bb38f91d18b04b9ba6d-566f0.jpg",
+              "suitableForDiet": "http://schema.org/GlutenFreeDiet",
+              "nutrition": {
+            "@type": "NutritionInformation",
+                         "calories": "573 calories",
+                          "fatContent": "25 grams",
+                          "carbohydrateContent": "26 grams",
+                          "proteinContent": "61 grams"
+                        },
+              "offers": {
+            "@type": "Offer",
+                          "price": "7.95",
+                          "priceCurrency": "USD"
             }
-          },
-          {
-           "@type": "Offer",
-           "price": "9.95",
-           "priceCurrency": "USD",
-           "eligibleQuantity" : {
-            "@type": "QuantitativeValue",
-            "name": "Bowl"
+             }
+             ]
+            },
+            {
+             "@type": "MenuSection",
+             "name": "Soups",
+             "hasMenuItem": [
+             {
+              "@type": "MenuItem",
+              "name": "Lobster Bisque",
+              "offers": [
+              {
+               "@type": "Offer",
+               "price": "6.75",
+               "priceCurrency": "USD",
+               "eligibleQuantity": {
+                "@type": "QuantitativeValue",
+                "name": "Cup"
+                }
+              },
+              {
+               "@type": "Offer",
+               "price": "9.95",
+               "priceCurrency": "USD",
+               "eligibleQuantity" : {
+                "@type": "QuantitativeValue",
+                "name": "Bowl"
+                }
+              }
+              ]
+             },
+             {
+              "@type": "MenuItem",
+              "name": "Creole Seafood Gumbo",
+              "offers": [
+              {
+               "@type": "Offer",
+               "price": "6.75",
+               "priceCurrency": "USD",
+               "eligibleQuantity": {
+                "@type": "QuantitativeValue",
+                "name": "Cup"
+                }
+              },
+              {
+               "@type": "Offer", 
+               "name": "Bowl",
+               "price": "9.95",
+               "priceCurrency": "USD",
+               "eligibleQuantity" : {
+                "@type": "QuantitativeValue",
+                "name": "Bowl"
+                }
+              }
+              ]
+             }
+             ]
+            },
+            {
+             "@type": "MenuSection",
+             "name": "Pastas",
+             "description": "Entrées served with dinner salad or a cup of soup of the day.",
+             "hasMenuItem": [
+             {
+              "@type": "MenuItem",
+              "name": "Veal Parmigiana",
+              "description": "Tender cuts of paneed veal crowned with golden fried eggplant, Italian red gravy, mozzarella, and parmesan; served with spaghetti.",
+              "offers": {
+               "@type": "Offer",
+               "price": "17.95",
+               "priceCurrency": "USD"
+              }
+             },
+             {
+              "@type": "MenuItem",
+              "name": "Eggplant Parmigiana",
+              "description": "Pan fried eggplant layered and topped with Italian red gravy, mozzarella, and parmesan baked until bubbly; served with spaghetti.",
+              "offers": {
+               "@type": "Offer",
+               "price": "14.95",
+               "priceCurrency": "USD"
+              }
+             }
+             ]
             }
-          }
-          ]
-         },
-         {
-          "@type": "MenuItem",
-          "name": "Creole Seafood Gumbo",
-          "offers": [
-          {
-           "@type": "Offer",
-           "price": "6.75",
-           "priceCurrency": "USD",
-           "eligibleQuantity": {
-            "@type": "QuantitativeValue",
-            "name": "Cup"
-            }
-          },
-          {
-           "@type": "Offer", 
-           "name": "Bowl",
-           "price": "9.95",
-           "priceCurrency": "USD",
-           "eligibleQuantity" : {
-            "@type": "QuantitativeValue",
-            "name": "Bowl"
-            }
-          }
-          ]
-         }
-         ]
-        },
-        {
-         "@type": "MenuSection",
-         "name": "Pastas",
-         "description": "Entrées served with dinner salad or a cup of soup of the day.",
-         "hasMenuItem": [
-         {
-          "@type": "MenuItem",
-          "name": "Veal Parmigiana",
-          "description": "Tender cuts of paneed veal crowned with golden fried eggplant, Italian red gravy, mozzarella, and parmesan; served with spaghetti.",
-          "offers": {
-           "@type": "Offer",
-           "price": "17.95",
-           "priceCurrency": "USD"
-          }
-         },
-         {
-          "@type": "MenuItem",
-          "name": "Eggplant Parmigiana",
-          "description": "Pan fried eggplant layered and topped with Italian red gravy, mozzarella, and parmesan baked until bubbly; served with spaghetti.",
-          "offers": {
-           "@type": "Offer",
-           "price": "14.95",
-           "priceCurrency": "USD"
-          }
-         }
-         ]
-        }
-        ]
-     }
-  }
-// export const getGoogleRestaurant = () => {
-//     return {
-//         "@context": "http://schema.org/",
-//         "@type": "Restaurant",
-//         "acceptsReservations": "true",
-//         "servesCuisine": "Indian",
-//         "name": "Gekko",
-//         "address": "1 infy loop, Mercedes Park, Vic",
-//         "priceRange": "$$",
-//         "telephone": "454 5454 454",
-//         "image": "https://goofy-archimedes-763914.netlify.com/static/scandic-c90942cfae912bb38f91d18b04b9ba6d-566f0.jpg",
-//         "hasMenu": {
-//             "@type": "Menu",
-//             "name": "Dinner", 
-//             "hasMenuSection": [
-//             {
-//              "@type": "MenuSection",
-//              "name": "Appetizers",
-//              "hasMenuItem": [
-//                 {
-//                 "@type": "MenuItem",
-//                 "name": "Fried Eggplant",
-//                 "description": "Served with Italian red gravy.",
-//                 "offers": {
-//                     "@type": "Offer",
-//                                 "price": "7.95",
-//                                 "priceCurrency": "USD"
-//                     }
-//                 },
-//                 {
-//                 "@type": "MenuItem",
-//                 "name": "Fried Calamari",
-//                 "description": "Served with Italian red gravy or honey mustard.",
-//                     "image": "https://goofy-archimedes-763914.netlify.com/static/scandic-c90942cfae912bb38f91d18b04b9ba6d-566f0.jpg",
-//                 "suitableForDiet": "http://schema.org/GlutenFreeDiet",
-//                 "nutrition": {"@type": "NutritionInformation",
-//                             "calories": "573 calories",
-//                             "fatContent": "25 grams",
-//                             "carbohydrateContent": "26 grams",
-//                             "proteinContent": "61 grams"
-//                             },
-//                     "offers": {
-//                         "@type": "Offer",
-//                             "price": "7.95",
-//                             "priceCurrency": "USD"
-//                         }
-//                 }
-//              ]
-//             },
-//             {
-//              "@type": "MenuSection",
-//              "name": "Soups",
-//              "hasMenuItem": [
-//              {
-//               "@type": "MenuItem",
-//               "name": "Lobster Bisque",
-//               "offers": [
-//               {
-//                "@type": "Offer",
-//                "price": "6.75",
-//                "priceCurrency": "USD",
-//                "eligibleQuantity": {
-//                 "@type": "QuantitativeValue",
-//                 "name": "Cup"
-//                 }
-//               },
-//               {
-//                "@type": "Offer",
-//                "price": "9.95",
-//                "priceCurrency": "USD",
-//                "eligibleQuantity" : {
-//                 "@type": "QuantitativeValue",
-//                 "name": "Bowl"
-//                 }
-//               }
-//               ]
-//              },
-//              {
-//               "@type": "MenuItem",
-//               "name": "Creole Seafood Gumbo",
-//               "offers": [
-//               {
-//                "@type": "Offer",
-//                "price": "6.75",
-//                "priceCurrency": "USD",
-//                "eligibleQuantity": {
-//                 "@type": "QuantitativeValue",
-//                 "name": "Cup"
-//                 }
-//               },
-//               {
-//                "@type": "Offer", 
-//                "name": "Bowl",
-//                "price": "9.95",
-//                "priceCurrency": "USD",
-//                "eligibleQuantity" : {
-//                 "@type": "QuantitativeValue",
-//                 "name": "Bowl"
-//                 }
-//               }
-//               ]
-//              }
-//              ]
-//             }
-//         ]
-        
-//     }
+            ]
+    }
     
-// }}
+}}

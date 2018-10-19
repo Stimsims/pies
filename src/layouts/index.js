7039a681ff0c9df4d2b4e636f21bf77217d082b5
illulli <illu.development@gmail.com>
2018-10-19 18:21:18 +1100

    added cookie consent

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,7 @@ import Transition from "./../components/Transition.js"
 import Header from './../components/header.js';
 import { StaticQuery, graphql } from 'gatsby';
 import styled, {ThemeProvider} from 'styled-components';
+import CookieConsent from "react-cookie-consent";
 
 import './../components/animations/slides.css';
 import './../components/animations/fades.css';
@@ -42,14 +43,26 @@ const TemplateWrapper = (props) => {
                                 title="Gatsby Default Starter"
                                 meta={[
                                 { name: `description`, content: `Sample` },
-                                { name: `keywords`, content: `sample, something` },
-                                {name: 'robots', content: 'noIndex'}
+                                { name: `keywords`, content: `sample, something` }
                                 ]}
                             >
                             </Helmet>
+                            <CookieConsent
+                                location="top"
+                                buttonText="Okay"
+                                cookieName="gekko-cookie-consent"
+                                style={{ background: "rgba(0,0,0,0.7)",  zIndex: '999999' }}
+                                buttonStyle={{ color: "#4e503b", fontSize: "13px", 
+                                animationName: 'wiggle', animationDuration: '2.5s', animationIterationCount: 'infinite',
+                                animationTimingFunction: 'ease-in-out' }}
+                                expires={150}
+                            >
+                                This website uses cookies to enhance its service to you.
+                            </CookieConsent>
                             <Header siteTitle={stat.site.siteMetadata.title} 
                                         image={data && data.headerImage? data.headerImage.childImageSharp.fluid:
                                         stat.headerImage.childImageSharp?stat.headerImage.childImageSharp.fluid:null} />
+                            
                             <div
                                 style={{
                                 margin: `0 auto`,

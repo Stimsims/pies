462c5091b19b17fafaebd8d847f99d30c3f09ed9
illulli <illu.development@gmail.com>
2018-10-21 18:21:48 +1100

    added netlify privacy page 2

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,1 +1,8 @@
-undefined
+import React from 'react';
+
+export default class Privacy extends React.Component{
+    render(){
+        console.log(`Privacy page props`, this.props);
+        return <p>Privacy</p>
+    }
+}
